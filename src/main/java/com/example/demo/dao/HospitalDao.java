3e9596631054bd695288957535524d0d4a50a820
sujin0607 <goweg0607@gmail.com>
2022-11-01 15:22:43 +0900

    Feat: HospitalDao에 findById와 테스트 메서드 추가

--- src/main/java/com/example/demo/dao/HospitalDao.java
+++ src/main/java/com/example/demo/dao/HospitalDao.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Component
 public class HospitalDao {
@@ -17,6 +18,31 @@ public class HospitalDao {
     public HospitalDao(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
+
+    private RowMapper<Hospital> hospitalMapper = new RowMapper<Hospital>() {
+        @Override
+        public Hospital mapRow(ResultSet rs, int rowNum) throws SQLException {
+            Hospital hospital = new Hospital(
+                rs.getInt("id"),
+                rs.getString("open_service_name"),
+                rs.getInt("open_local_government_code"),
+                rs.getString("management_number"),
+                rs.getTimestamp("license_date").toLocalDateTime(),
+                rs.getInt("business_status"),
+                rs.getInt("business_status_code"),
+                rs.getString("phone"),
+                rs.getString("full_address"),
+                rs.getString("road_name_address"),
+                rs.getString("hospital_name"),
+                rs.getString("business_type_name"),
+                rs.getInt("healthcare_provider_count"),
+                rs.getInt("patient_room_count"),
+                rs.getInt("total_number_of_beds"),
+                rs.getFloat("total_area_size")
+            );
+            return hospital;
+        }
+    };
 
     public void add(Hospital hospital){
         String sql = "INSERT INTO `likelion-db`.`hospital` (`id`, `open_service_name`, `open_local_government_code`, `management_number`, `license_date`, `business_status`, `business_status_code`, `phone`, `full_address`, `road_name_address`, `hospital_name`, `business_type_name`, `healthcare_provider_count`, `patient_room_count`, `total_number_of_beds`, `total_area_size`) " +
@@ -49,32 +75,11 @@ public class HospitalDao {
         String sql = "SELECT COUNT(*) FROM hospital";
         return this.jdbcTemplate.queryForObject(sql, Integer.class);
     }
-    public Hospital findById(int id){
-        String sql = "SELECT * FROM hospital WHERE id = ?";
-        RowMapper<Hospital> rowMapper = new RowMapper<Hospital>() {
-            @Override
-            public Hospital mapRow(ResultSet rs, int rowNum) throws SQLException {
-                Hospital hospital = new Hospital();
-                hospital.setId(rs.getInt("id"));
-                hospital.setOpenServiceName(rs.getString("open_service_name"));
-                hospital.setOpenLocalGovernmentCode(rs.getInt("open_local_government_code"));
-                hospital.setManagementNumber(rs.getString("management_number"));
-                hospital.setLicenseDate(rs.getTimestamp("license_date").toLocalDateTime());
-                hospital.setBusinessStatus(rs.getInt("business_status"));
-                hospital.setBusinessStatusCode(rs.getInt("business_status_code"));
-                hospital.setPhone(rs.getString("phone"));
-                hospital.setFullAddress(rs.getString("full_address"));
-                hospital.setRoadNameAddress(rs.getString("road_name_address"));
-                hospital.setHospitalName(rs.getString("hospital_name"));
-                hospital.setBusinessTypeName(rs.getString("business_type_name"));
-                hospital.setHealthcareProviderCount(rs.getInt("healthcare_provider_count"));
-                hospital.setPatientRoomCount(rs.getInt("patient_room_count"));
-                hospital.setTotalNumberOfBeds(rs.getInt("total_number_of_beds"));
-                hospital.setTotalAreaSize(rs.getInt("total_area_size"));
 
-                return hospital;
-            }
-        };
-        return this.jdbcTemplate.queryForObject(sql, rowMapper, id);
+    public Hospital findById(int id){
+        String sql = "select * from hospital where id = ?";
+        return this.jdbcTemplate.queryForObject(sql, this.hospitalMapper , id);
     }
+
+
 }
